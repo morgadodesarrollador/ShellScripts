e665f7d3a2540ee4a7fd6935e35dfe81c2896ff6
morgadodesarrollador <morgadodesarrollador@gmail.com>
2023-01-16 14:11:00 +0100

    script 01-prueba.sh finalizado

--- prueba.sh
+++ prueba.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
 echo "Ejecución de prueba.sh de forma remota por jenkins"
 echo "Bienvenido Jenkins por facilitarnos la vida..."
+for i in 1 2 3 4 5
+do
+   echo "Welcome $i times"
+done
