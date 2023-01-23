787f3fbfe8ddab253990ce291cbbb156356decd9
morgadodesarrollador <morgadodesarrollador@gmail.com>
2023-01-23 13:09:48 +0100

    1-Ejecucion Job mediante groovy

--- dsljob.groovy
+++ dsljob.groovy
@@ -1,17 +1,20 @@
 job('groovy-Job-DSL') {
     description('Job DSL YYY por Groovy desde GitHub')
-    scm {
-        git('https://github.com/morgadodesarrollador/ShellScripts.git', 'master'){ 
-            node -> 
-                node / gitConfigName('morgadodesarrollador')
-                node / gitConfigEmail('morgadodesarrollador@gmail.com')
-        }
-    }
-    triggers {
-        githubPush()
-    }
-    
-    steps {
-        shell("bash install.sh")
-    }
 }
+
+
+    // scm {
+    //     git('https://github.com/morgadodesarrollador/ShellScripts.git', 'master'){ 
+    //         node -> 
+    //             node / gitConfigName('morgadodesarrollador')
+    //             node / gitConfigEmail('morgadodesarrollador@gmail.com')
+    //     }
+    // }
+    // triggers {
+    //     githubPush()
+    // }
+    
+    // steps {
+    //     shell("bash install.sh")
+    // }
+//}
