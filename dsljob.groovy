a82f50d34d7445e4ee9180a56c958c6c001cff77
morgadodesarrollador <morgadodesarrollador@gmail.com>
2023-01-23 10:15:19 +0100

    1-Ejecucion Job mediante groovy

--- dsljob.groovy
+++ dsljob.groovy
@@ -1,5 +1,5 @@
 job('groovy-Job-DSL') {
-    description('Job DSL creado por Groovy desde GitHub')
+    description('Job DSL  por Groovy desde GitHub')
     scm {
         git('https://github.com/morgadodesarrollador/ShellScripts.git', 'master'){ 
             node -> 
